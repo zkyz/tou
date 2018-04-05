c26d65970f29e2596e19e8b1c02f75ee6b9d9462
zkyz <hellozkyz@gmail.com>
2018-04-06 00:32:38 +0900

    tou movement #1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-tabs */
 import $ from 'jquery'
-import 'jquery-ui/ui/widgets/draggable'
+import 'jquery-ui/ui/widgets/droppable'
 import 'jquery-ui/ui/widgets/sortable'
 import 'jquery-ui/ui/disable-selection'
 import './tou.scss'
@@ -61,9 +61,90 @@ import './tou.scss'
   })
 
   const touMoveHandler = $('<div class="tou-move-handle"/>')
+  touMoveHandler.event = {
+    start: function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+
+      touMoveHandler.moving = true
+      touMoveHandler.target = $(this).closest('.tou').addClass('tou-moving')
+      touMoveHandler.start = [e.pageX, e.pageY]
+
+      $('.tou-gap')
+        .on('mouseenter', touMoveHandler.event.placeholder.enter)
+        .on('mouseleave', touMoveHandler.event.placeholder.leave)
+
+      $('body')
+        .on('mousemove', touMoveHandler.event.move)
+        .on('mouseup', touMoveHandler.event.end)
+
+      $('.tou-list').addClass('tou-movement')
+    },
+    move: function (e) {
+      const distance = [
+        e.pageX - touMoveHandler.start[0],
+        e.pageY - touMoveHandler.start[1]
+      ]
+      touMoveHandler.target.css('transform', `translate(${distance[0]}px, ${distance[1]}px) scale(.5, .5) rotate(-5deg)`)
+    },
+    placeholder: {
+      enter: function () {
+        touMoveHandler.destination = $(this).addClass('tou-ready-drop')
+      },
+      leave: function () {
+        $(this).removeClass('tou-ready-drop')
+        touMoveHandler.destination = null
+      }
+    },
+    end: function () {
+      touMoveHandler.moving = false
+
+      if (touMoveHandler.destination) {
+        const dataWidth = touMoveHandler.destination.attr('data-width')
+
+        touMoveHandler.destination
+          .removeAttr('data-width')
+          .removeClass('tou-ready-drop')
+          .before(touMoveHandler.target)
+
+        touMoveHandler.target
+          .removeClass('tou-moving')
+          .css({
+            'transition': 'none',
+            'transform': 'none'
+          })
+          .attr('data-width', dataWidth)
+          .before('<div class="tou tou-gap"/>')
+      } else {
+        touMoveHandler.target
+          .removeClass('tou-moving')
+          .css({
+            'transition': 'transform .4s',
+            'transform': 'none'
+          })
+
+        setTimeout(() => touMoveHandler.target.css('transition', 'none'), 500)
+      }
+
+      $('.tou-gap')
+        .off('mouseenter', touMoveHandler.event.placeholder.enter)
+        .off('mouseleave', touMoveHandler.event.placeholder.leave)
+
+      $('body')
+        .off('mousemove', touMoveHandler.event.move)
+        .off('mouseup', touMoveHandler.event.end)
+
+      $('.tou-list').removeClass('tou-movement')
+    }
+  }
+
+  touMoveHandler.on('mousedown', touMoveHandler.event.start)
+
   $('.tou')
-    .on('mouseenter', function() {
-      $(this).append(touMoveHandler)
+    .on('mouseenter', function () {
+      if (!touMoveHandler.moving) {
+        $(this).append(touMoveHandler)
+      }
     })
     .on('click', function () {
       sizeHandle.tou && sizeHandle.tou.removeClass('tou-selected')
