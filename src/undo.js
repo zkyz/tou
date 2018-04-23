2f1b793d558a96cde0af9156cefe6771cff624e4
zkyz <hellozkyz@gmail.com>
2018-04-24 00:10:48 +0900

    undo redo #2

--- src/undo.js
+++ src/undo.js
@@ -1,37 +1,74 @@
-export const records = []
-export let recordIndex = 0
+export const TYPES = {
+  TEXT: 'TEXT'
+}
 
-const redo = function () {
-  const record = records[recordIndex]
-  recordIndex ++
+const records = []
+let index = 0
 
-  if (recordIndex >= records.length) {
-    recordIndex = records.length - 1
+export function save (type, props) {
+  records.splice(index)
+
+  const record = records[index - 1]
+
+  if (record &&
+    record.type === type &&
+    record.value === props.value) {
     return
   }
 
-  if (record.type === 'INPUT') {
-    record.element.innerHTML = record.value[1]
+  records.push({
+    type,
+    ...props
+  })
+
+  index++
+}
+
+const redo = function () {
+  if (index === 0) {
+    index++
+  }
+
+  const record = records[index++]
+
+  if (record) {
+    switch (record.type) {
+    case TYPES.TEXT:
+      console.log(record)
+      // record.element.innerHTML = record.after
+    }
+  }
+
+  if (index > records.length) {
+    index = records.length
   }
 }
 
 const undo = function () {
-  const record = records[recordIndex]
-  recordIndex --
-
-  if (recordIndex < 1) {
-    recordIndex = 0
-    return
+  if (index === records.length) {
+    index--
   }
 
-  if (record.type === 'INPUT') {
-    record.element.innerHTML = record.value[0]
+  const record = records[--index]
+
+  if (record) {
+    switch (record.type) {
+    case TYPES.TEXT:
+      console.log(record)
+      // record.element.innerHTML = record.before
+      break
+    }
+  }
+
+  if (index < 0) {
+    index = 0
   }
 }
 
 document.body.addEventListener('keydown', function (e) {
-  if (e.keyCode === 90) {
+  if (e.ctrlKey && e.keyCode === 90) {
     e.preventDefault()
+    e.stopPropagation()
 
     if (e.shiftKey) {
       redo()
@@ -42,18 +79,8 @@ document.body.addEventListener('keydown', function (e) {
   }
 })
 
-let keyupTimer,
-  beforeValue
-
-document.body.addEventListener('keyup', function (e) {
-  clearTimeout(keyupTimer)
-  keyupTimer = setTimeout(function () {
-    records.push({
-      type: 'INPUT',
-      element: e.target,
-      value: [beforeValue, e.target.innerHTML]
-    })
-
-    beforeValue = e.target.innerHTML
-  }, 1000)
+document.body.addEventListener('keypress', function (e) {
+  save(TYPES.TEXT, {
+    value: e.keyCode
+  })
 })
